c6015365f392ebbdcdcc0c607cc39ed7803d203e
Amanda Ripley <amanda.jade@hotmail.com>
2020-06-02 19:55:35 -0500

    Plot1 commit

---
+++ Plot1.R
@@ -0,0 +1,6 @@
+data <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+hpc <- subset(data, Date %in% c("1/2/2007","2/2/2007"))
+gap <- as.numeric(hpc$Global_active_power)
+png("Plot1.png", width=480, height=480)
+hist(gap, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="Red")
+dev.off()
